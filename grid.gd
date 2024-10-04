07d98be14f1a804e5874019b0a5305f4e88bebae
DylanLem <lemcke.dl@gmail.com>
2024-10-04 16:48:33 -0600

    added stubs

---
+++ grid.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+enum Direction {North, South, East, West}
+
+var width: int;
+var height: int;
+
+var contents: Array[Array];
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func get_neighbour(cell: Cell, direction: Direction) -> Cell:
+	
+	return Cell.new();
--- new_script.gd
+++ new_script.gd
@@ -1,11 +1,17 @@
-extends Node
+
+ANTHILL BATTLES
+side view
+produce ants and stuff in your base
+automatically send ant warriors to enemy
+NPC enemies
+buildings of various shapes
+ant upgrades?
+make ants
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+game bones
+- grid system
+- buildings that fit to grid
+- tetris shaped buildings
+- place buildings of same type adjacent to eachother for bonus
+- buildings must be placed next to other buildings
